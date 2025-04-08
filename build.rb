eab0f2b92a9d4c426fd6dfc675f1e33d3307c7ea
Phil Hofmann <phil@200ok.ch>
2025-04-08 11:21:21 +0200

    Revert "support multiple currencies"

--- build.rb
+++ build.rb
@@ -4,7 +4,6 @@ require 'yaml'
 require 'erb'
 require 'ostruct'
 require 'json'
-require 'csv'
 
 base = File.dirname(__FILE__)
 
@@ -15,105 +14,6 @@ config = File.expand_path('raisenow.yml', base)
 # setting of the tamaro widget, wtf?
 # data = YAML.unsafe_load(File.read(config))
 data = YAML.load(File.read(config))
-
-# read the pricing sheet
-sheet = File.expand_path('pricing.csv', base)
-csv = CSV.read(sheet, headers: true)
-puts  "Processing pricing sheet: #{csv.count} lines"
-pricing = csv.reduce({}) do |aggr, row|
-  form = (row["Formular"] || "default").strip
-  variant = (row["Variante"] || ".").strip
-  caption = (row["Beschriftung"] || ".").strip
-  aggr[form] ||= {}
-  aggr[form][variant] ||= []
-  aggr[form][variant] << {
-    chf: row["CHF"],
-    eur: row["EUR"],
-    usd: row["USD"],
-    caption: caption
-  }
-  aggr
-end
-
-# predefined currencies to support
-CURRENCIES = [:chf, :eur, :usd]
-
-# predefined conditional clauses, which are referenced in column "Variante"
-CONDITIONALS = {
-  "grasspaper" => "paymentForm(stored_customer_certificate) == grasspaper",
-  "digital"    => "paymentForm(stored_customer_certificate) == digital",
-  "none"       => "paymentForm(stored_customer_certificate) == none",
-  # rnwPatenschaftenAuswahl
-  "einmalig"   => "paymentType() == onetime",
-  "monatlich"  => "paymentType() == recurring and recurringInterval() == monthly",
-  "jährlich"   => "paymentType() == recurring and recurringInterval() == yearly"
-}
-
-def compile_amounts(variants)
-  return if variants.nil?
-  if variants.count == 1
-    variant = variants.values.first
-    CURRENCIES.map do |currency|
-      { "if" => "currency() == #{currency}",
-        "then" => variant.map { |v| v[currency].to_f }.sort }
-    end
-  else
-    variants.map do |kv|
-      variant_name, variant = kv
-      CURRENCIES.map do |currency|
-        { "if" => "currency() == #{currency} and #{CONDITIONALS[variant_name]}",
-          "then" => variant.map { |v| v[currency].to_f }.sort }
-      end
-    end.flatten
-  end
-end
-
-def compile_minimumCustomAmount(amounts)
-  amounts.map do |amount|
-    amount["then"] = [amount["then"].first]
-    amount
-  end
-end
-
-def compile_amountDescriptions(variants)
-  [].tap do |result|
-    variants.each do |kv|
-      variant_name, amounts = kv
-      CURRENCIES.each do |currency|
-        amounts.each do |amount|
-          next if amount[:caption] == '.'
-          result << {
-            "if" => ["currency() == #{currency}",
-                     "amount() == #{amount[currency]}",
-                     variant_name != '.' ? CONDITIONALS[variant_name] : nil].compact.join(" and "),
-            "then" => ["#{amount[:caption]}"]
-          }
-        end
-      end
-    end
-  end
-end
-
-# walk tamaro forms and update amounts and other properties
-# based on sheet
-form_names = data["tamaro"].keys
-form_names.each do |form_name|
-  form = data["tamaro"][form_name]
-  variants= pricing[form_name]
-  if variants.nil?
-    warn "Missing pricing for #{form_name}"
-  else
-    puts "Found #{variants.count} variants for #{form_name}"
-    amounts = compile_amounts(variants)
-    form["amounts"] = amounts
-    form["minimumCustomAmount"] = compile_minimumCustomAmount(amounts)
-    form["translations"]["de"]["amount_descriptions"] = compile_amountDescriptions(variants)
-    data["tamaro"][form_name] = form
-  end
-end
-
-# puts "=" * 80
-# exit
 
 class ErbBinding < OpenStruct
   def get_binding
